806926283e36176978ccbd2f695e5630c412209d
lbrion <lbrion@stanford.edu>
2014-06-06 20:42:57 -0700

    added all of the diet jade pages

--- server.js
+++ server.js
@@ -41,6 +41,50 @@ app.get('/diets/diabetes', function (req, res) {
   res.render('diabetes', { appTitle: 'MedHealthDiets: your source for medical diet information' });
 });
 
+app.get('/diets/highbloodpressure', function (req, res) {
+  res.render('highbloodpressure', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/alzheimers', function (req, res) {
+  res.render('alzheimers', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/eyesight', function (req, res) {
+  res.render('eyesight', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/celiacs', function (req, res) {
+  res.render('celiacs', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/cystinuria', function (req, res) {
+  res.render('cystinuria', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/heart', function (req, res) {
+  res.render('heart', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/kidney', function (req, res) {
+  res.render('kidney', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/liver', function (req, res) {
+  res.render('liver', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/lowsodium', function (req, res) {
+  res.render('lowsodium', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/thyroid', function (req, res) {
+  res.render('thyroid', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
+app.get('/diets/weightloss', function (req, res) {
+  res.render('weightloss', { appTitle: 'MedHealthDiets: your source for medical diet information' })
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
